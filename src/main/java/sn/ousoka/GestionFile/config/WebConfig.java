d5867a11280e07d6e9f33ac89e75041c0a230dd3
Ousmane KA <102234876+Ousoka@users.noreply.github.com>
2025-02-20 16:40:17 +0000

    okok

---
+++ src/main/java/sn/ousoka/GestionFile/config/WebConfig.java
@@ -0,0 +1,19 @@
+package sn.ousoka.GestionFile.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver jspViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setOrder(1); // Ensure JSP resolver priority if multiple resolvers exist
+        return resolver;
+    }
+}
